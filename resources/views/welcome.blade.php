a758ffe411f0d92f86a293013fa1a65bb512c8d8
bintangdilangit <syekhuda463@gmail.com>
2021-11-17 18:30:20 +0700

    final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,9 +45,9 @@
                         <div class="member">
                             <div class="member-img">
 
-                                <a class="example-image-link" href="{{ asset('event/171121qep.jpg') }}"
+                                <a class="example-image-link" href="{{ asset('event/171121qep.png') }}"
                                     data-lightbox="example-1"><img class="example-image"
-                                        src="{{ asset('event/171121qep.jpg') }}" style="width:100%;max-width:300px"
+                                        src="{{ asset('event/171121qep.png') }}" style="width:100%;max-width:300px"
                                         alt="image-1" /></a>
 
                             </div>
